6a6b49e68f52858fa57ca1e9c630e224a8778041
Hoang Tuan <nhtuan212@gmail.com>
2023-12-10 13:00:44 +0700

    Update env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, TypeScript Express!");
