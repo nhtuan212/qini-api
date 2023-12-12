045a3e35a7cd9fb5e6723ee5fd69ba669b459809
Hoang Tuan <nhtuan212@gmail.com>
2023-12-12 19:38:41 +0700

    Update root deploy

--- api/router/index.ts
+++ api/router/index.ts
@@ -2,10 +2,10 @@ import { homePage } from "./homePage";
 // import { user, userSlug } from "./user";
 
 export const router = (app: any) => {
-    app.get("/", (req: any, res: any) => {
-        console.log("=> ", "Database connected successfully!");
-        res.send("Hello World!");
-    });
+    // app.get("/", (req: any, res: any) => {
+    //     console.log("=> ", "Database connected successfully!");
+    //     res.send("Hello World!");
+    // });
 
     app.use("/api", homePage);
     // app.use("/api/user", user);
