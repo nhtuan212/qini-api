6dfa2a1be16ed73d08199b9c2cbfc7efce5fcac7
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 14:54:38 +0700

    Update main file deploy

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,44 @@
+import "dotenv/config";
 import express from "express";
-import { v4 } from "uuid";
+import { router } from "../src/router";
+import { dbConnection } from "../src/service";
+
 const app = express();
+const port = process.env.PORT || 8000;
 
-app.get("/api", (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+router(app);
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    dbConnection()
+        .then(() => {
+            console.log("Database connected successfully!");
+        })
+        .then(() => {
+            console.log(`Example app listening on port ${port}`);
+        })
+        .catch(err => {
+            throw err;
+        });
+
+    // console.log(`Example app listening on port ${port}`);
 });
 
-app.get("/api/item/:slug", (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
+// import express from "express";
+// import { v4 } from "uuid";
+// const app = express();
 
-module.exports = app;
+// app.get("/api", (req, res) => {
+//     const path = `/api/item/${v4()}`;
+//     res.setHeader("Content-Type", "text/html");
+//     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
+
+// app.get("/api/item/:slug", (req, res) => {
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug}`);
+// });
+
+// module.exports = app;
