2df1c34c8f380bd71abf9d6a71b985c79d51f8b9
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 16:13:10 +0700

    Update main file deploy

--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,13 @@ import express from "express";
 const app = express();
 import { router } from "../src/router";
 
-app.get("/api", (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.json({ message: "Hello World" });
-});
-
-app.use(express.static("public"));
+// app.get("/api", (req, res) => {
+//     res.setHeader("Content-Type", "text/html");
+//     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+//     res.json({ message: "Hello World" });
+// });
 
 router(app);
+app.use(express.static("public"));
 
 module.exports = app;
