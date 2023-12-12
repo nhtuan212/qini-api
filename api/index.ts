c48572b0a1c192a8aa23217b0ceae9d30f7e48fd
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 16:04:30 +0700

    Update main file deploy

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,12 @@
 import express from "express";
-// import { v4 } from "uuid";
 const app = express();
 import { router } from "../src/router";
 
 app.get("/api", (req, res) => {
-    // const path = `/api/item/${v4()}`;
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
     res.json({ message: "Hello World" });
-    // res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
-
-// app.get("/api/item/:slug", (req, res) => {
-//     const { slug } = req.params;
-//     res.end(`Item: ${slug}`);
-// });
 
 app.use(express.static("public"));
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { homePage } from "./homePage";
-// import { user, userSlug } from "./user";
+import { user, userSlug } from "./user";
 
 export const router = (app: any) => {
     app.use("/api/home", homePage);
-    // app.use("api/user", user);
-    // app.use("api/user/a", userSlug);
+    app.use("/api/user", user);
+    app.use("/api/user/a", userSlug);
 };
