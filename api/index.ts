98e4188ef355c38cef66e59864016f20121511af
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 14:59:48 +0700

    Update main file deploy

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,17 @@
 import "dotenv/config";
 import express from "express";
+// import { v4 } from "uuid";
 import { router } from "../src/router";
 import { dbConnection } from "../src/service";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get("/api", (req, res) => {
-    const path = `/api/item/${v4()}`;
+app.get("/", (req, res) => {
+    // const path = `/api/item/${v4()}`;
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    // res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
 app.get("/api/item/:slug", (req, res) => {
