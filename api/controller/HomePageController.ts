2b35e8d854037071a80ddbe7a4d6259db2865150
Hoang Tuan <nhtuan212@gmail.com>
2023-12-12 19:42:29 +0700

    Update root deploy

--- api/controller/HomePageController.ts
+++ api/controller/HomePageController.ts
@@ -5,6 +5,8 @@ export const HomePageController = (req: Request, res: Response) => {
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
 
+    console.log("=> ", "Database connected successfully!");
+
     res.json({
         message: "Hello, TypeScript Express!",
     });
--- api/router/index.ts
+++ api/router/index.ts
@@ -2,14 +2,6 @@ import { homePage } from "./homePage";
 // import { user, userSlug } from "./user";
 
 export const router = (app: any) => {
-    app.use(
-        "/",
-        app.get("/", (req: any, res: any) => {
-            console.log("=> ", "Database connected successfully!");
-            res.send("Hello World!");
-        }),
-    );
-
     app.use("/api", homePage);
     // app.use("/api/user", user);
     // app.use("/api/user/a", userSlug);
