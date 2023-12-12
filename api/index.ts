8a64a5ad6d26e077b8b1aa9e632a19e6fcf75ff5
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 15:11:29 +0700

    Update main file deploy

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,8 @@
-import "dotenv/config";
 import express from "express";
 import { v4 } from "uuid";
-
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     const path = `/api/item/${v4()}`;
     res.setHeader("Content-Type", "text/html");
     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
@@ -15,40 +13,5 @@ app.get("/api/item/:slug", (req, res) => {
     const { slug } = req.params;
     res.end(`Item: ${slug}`);
 });
-
-// router(app);
-
-// app.use(express.static("public"));
-
-// app.listen(port, () => {
-//     dbConnection()
-//         .then(() => {
-//             console.log("Database connected successfully!");
-//         })
-//         .then(() => {
-//             console.log(`Example app listening on port ${port}`);
-//         })
-//         .catch(err => {
-//             throw err;
-//         });
-
-//     // console.log(`Example app listening on port ${port}`);
-// });
-
-// import express from "express";
-// import { v4 } from "uuid";
-// const app = express();
-
-// app.get("/api", (req, res) => {
-//     const path = `/api/item/${v4()}`;
-//     res.setHeader("Content-Type", "text/html");
-//     res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
-
-// app.get("/api/item/:slug", (req, res) => {
-//     const { slug } = req.params;
-//     res.end(`Item: ${slug}`);
-// });
 
 module.exports = app;
