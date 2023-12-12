042979dc35b892668fa02e2ef4188b73eab2085c
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 14:12:54 +0700

    Update connect vercel postgres database

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,4 +1,15 @@
 -- CreateTable
+CREATE TABLE "posts" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER,
+    "title" VARCHAR(100) NOT NULL,
+    "content" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "username" VARCHAR(50) NOT NULL,
@@ -6,6 +17,7 @@ CREATE TABLE "users" (
     "email" VARCHAR(50) NOT NULL,
     "create_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "update_at" TIMESTAMP(6),
+    "active" BOOLEAN NOT NULL DEFAULT true,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -15,4 +27,7 @@ CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
