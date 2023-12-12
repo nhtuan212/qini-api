0ac052016fe17def2cf3800f27d0633f0f027eff
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2023-12-12 15:27:13 +0700

    Update main file deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { homePage } from "./homePage";
 // import { user, userSlug } from "./user";
 
 export const router = (app: any) => {
-    app.use("/api/home", homePage);
+    app.use("/", homePage);
     // app.use("/user", user);
     // app.use("/user/a", userSlug);
 };
