648fcc787a3c23f8bda6d9667e0a66e843c76957
Hoang Tuan <nhtuan212@gmail.com>
2023-12-12 19:44:10 +0700

    Update root deploy

--- api/controller/HomePageController.ts
+++ api/controller/HomePageController.ts
@@ -2,11 +2,6 @@ import { Request, Response } from "express";
 
 //** [GET]/ */
 export const HomePageController = (req: Request, res: Response) => {
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-
-    console.log("=> ", "Database connected successfully!");
-
     res.json({
         message: "Hello, TypeScript Express!",
     });
