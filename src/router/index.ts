9fb49f4ec360bd7da5713158414e47475c655309
Hoang Tuan <nhtuan212@gmail.com>
2024-01-10 21:06:34 +0700

    Update source

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,31 @@ export const router = (app: any) => {
     //     next();
     // });
 
+    // Add headers before the routes are defined
+    app.use((req: any, res: any, next: any) => {
+        // Website you wish to allow to connect
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        // Request methods you wish to allow
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+        );
+
+        // Request headers you wish to allow
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "X-Requested-With,content-type",
+        );
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader("Access-Control-Allow-Credentials", 1);
+
+        // Pass to next layer of middleware
+        next();
+    });
+
     app.use("/", home);
     app.use("/user", user);
 };
--- src/router/user.ts
+++ src/router/user.ts
@@ -4,4 +4,4 @@ import { GetUser, CreateUser } from "../controller/UserController";
 const router = express.Router();
 
 export const user = router.get("/", GetUser);
-router.get("/create", CreateUser);
+router.post("/create", CreateUser);
