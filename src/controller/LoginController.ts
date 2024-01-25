ca139fa9b818fbd7d924f68968cb776c2854ff26
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-01-25 09:46:28 +0700

    Refactor all route with method

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -3,21 +3,31 @@ import { login } from "../service/loginService";
 
 //** [POST]/login */
 export const Login = async (req: Request, res: Response) => {
-    return await login({
-        username: req.body.username,
-        password: req.body.password,
-    })
-        .then(resData => {
-            // Destructure data
-            const { code, message, data } = resData as any;
+    switch (req.method) {
+        //** POST */
+        case "POST":
+            return await login({
+                username: req.body.username,
+                password: req.body.password,
+            })
+                .then(resData => {
+                    // Destructure data
+                    const { code, message, data } = resData as any;
 
-            return res.status(code).json({
-                code,
-                message,
-                data,
+                    return res.status(code).json({
+                        code,
+                        message,
+                        data,
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
+
+        //** Default */
+        default:
+            return res.json({
+                message: "Sorry, something wrong method!",
             });
-        })
-        .catch(err => {
-            throw err;
-        });
+    }
 };
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,41 +1,48 @@
 import { Request, Response } from "express";
 import { createUser, getUser } from "../service/userService";
-import { Users } from "@prisma/client";
 
 //** [GET]/user */
-export const GetUser = async (req: Request, res: Response) => {
+export const User = async (req: Request, res: Response) => {
     const { offset, limit } = req.query;
     const { statusCode } = res;
 
-    return await getUser({ offset, limit })
-        .then(data => {
-            return res.status(statusCode).json({
-                statusCode,
-                data,
-            });
-        })
-        .catch(err => {
-            throw err;
-        });
-};
+    switch (req.method) {
+        //** GET */
+        case "GET":
+            return await getUser({ offset, limit })
+                .then(data => {
+                    return res.status(statusCode).json({
+                        statusCode,
+                        data,
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
 
-//** [GET]/user/create */
-// "?username=Binayu1&password=123&email=mail1@gmail.com"
-export const CreateUser = async (req: Request, res: Response) => {
-    return createUser({
-        query: req.query as Request["query"] & Users,
-    })
-        .then(resData => {
-            // Destructure data
-            const { code, message, data } = resData;
+        //** POST */
+        case "POST":
+            return createUser({
+                body: req.body,
+            })
+                .then(resData => {
+                    // Destructure data
+                    const { code, message, data } = resData;
 
-            return res.status(code).json({
-                code,
-                message,
-                data,
+                    return res.status(code).json({
+                        code,
+                        message,
+                        data,
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
+
+        //** Default */
+        default:
+            return res.json({
+                message: "Sorry, something wrong method!",
             });
-        })
-        .catch(err => {
-            throw err;
-        });
+    }
 };
