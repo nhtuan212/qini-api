6201a4324ae67ab29c5e363ed2185daafa5a5711
Nguyen Hoang Tuan <63133830+nhtuan212@users.noreply.github.com>
2024-04-19 16:18:33 +0700

    Update Hello text

--- src/controller/HomeController.ts
+++ src/controller/HomeController.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 //** [GET]/ */
 export const HomeController = (req: Request, res: Response) => {
     res.json({
-        message: "Hello, TypeScript Expresss!",
+        message: "Hello, TypeScript Express!",
     });
 };
