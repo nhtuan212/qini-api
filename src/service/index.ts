241f2601bb2a6a54e3b0e71f256fc04129e69a99
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-04-28 17:25:21 +0700

    Update connect posgres

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "../../dist/generated/client";
 export const client = new PrismaClient({
     datasources: {
         db: {
-            url: "postgresql://postgres:123@localhost:2222/qini_db",
+            url: process.env.DATABASE_URL,
         },
     },
     log: [
