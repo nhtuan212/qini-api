1f39d7d83094e3b41279c17464437b5ae2a3a1b9
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-04-28 16:59:42 +0700

    Update env and delete docker images dangling production

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from "../../dist/generated/client";
 
 export const client = new PrismaClient({
-    // datasources: {
-    //     db: {
-    //         url:
-    //             process.env.NODE_ENV === "production"
-    //                 ? process.env.PRODUCTION_DATABASE_URL
-    //                 : process.env.DATABASE_URL,
-    //     },
-    // },
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
     log: [
         {
             emit: "event",
