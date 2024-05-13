e9c84b365f2e78d0b7025f85f91499cbdb5064bf
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-05-13 14:16:43 +0700

    Update migrate postgres

--- prisma/migrations/20240504025035_relation_staff_report/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Reports" ADD COLUMN     "staffId" VARCHAR(50) NOT NULL DEFAULT '';
-
--- AddForeignKey
-ALTER TABLE "Reports" ADD CONSTRAINT "Reports_staffId_fkey" FOREIGN KEY ("staffId") REFERENCES "Staffs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
