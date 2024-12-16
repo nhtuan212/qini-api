5028b724a15942d25c9683726c6fb0a03940ca19
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-12-16 12:23:24 +0700

    Update cors allow *

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,7 @@ export const router = (app: Express) => {
         }),
     );
 
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV !== "production") {
         //** Cors */
         app.use(cors());
     }
