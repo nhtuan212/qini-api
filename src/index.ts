7674c2a67fe655f3328b89dea91a4e2da3eb66fb
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2025-07-02 16:42:29 +0900

    Feature cron job add target at 5:00 AM every day

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import "dotenv/config";
 import express from "express";
 import { router } from "./router";
 import { dbConnection } from "./service";
+import { createTarget } from "./service/targetService";
+import cron from "node-cron";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 router(app);
+
+cron.schedule("0 5 * * *", async () => {
+    //** Create target at 5:00 AM every day */
+    await createTarget({
+        body: {
+            name: "Doanh số",
+            target_at: new Date(),
+        } as any,
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
