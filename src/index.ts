d55343c419186531af89dc03776a169def6cecc0
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2025-07-11 08:54:50 +0900

    Update cron

--- src/index.ts
+++ src/index.ts
@@ -11,27 +11,39 @@ const port = process.env.PORT || 8000;
 
 router(app);
 
-cron.schedule("50 8 * * *", async () => {
-    console.log("Create target at 8:50 AM every day");
-    //** Create target at 5:00 AM every day */
-    await createTarget({
-        body: {
-            name: "Doanh số",
-            target_at: formatDate(new Date()),
-        } as any,
-    });
-});
+cron.schedule(
+    "58 8 * * *",
+    async () => {
+        console.log("Create target at 8:58 AM every day");
+        //** Create target at 5:00 AM every day */
+        await createTarget({
+            body: {
+                name: "Doanh số",
+                target_at: formatDate(new Date()),
+            } as any,
+        });
+    },
+    {
+        timezone: "Asia/Ho_Chi_Minh",
+    },
+);
 
-cron.schedule("50 6 * * *", async () => {
-    console.log("Create target at 6:50 AM every day");
-    //** Create target at 5:00 AM every day */
-    await createTarget({
-        body: {
-            name: "Doanh số",
-            target_at: formatDate(new Date()),
-        } as any,
-    });
-});
+cron.schedule(
+    "58 6 * * *",
+    async () => {
+        console.log("Create target at 6:58 AM every day");
+        //** Create target at 5:00 AM every day */
+        await createTarget({
+            body: {
+                name: "Doanh số",
+                target_at: formatDate(new Date()),
+            } as any,
+        });
+    },
+    {
+        timezone: "Asia/Ho_Chi_Minh",
+    },
+);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
