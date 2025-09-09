7b80155f18d02430947d48d49d564f08265250bc
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2025-09-09 17:07:16 +0900

    Update user schema

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -12,7 +12,7 @@ export const userTable = pgTable("user", {
     username: varchar("username", { length: 255 }).notNull(),
     password: varchar("password", { length: 255 }).notNull(),
     email: varchar("email", { length: 255 }),
-    active: boolean("active").notNull().default(true),
+    isActive: boolean("is_active").notNull().default(true),
     role: roleEnum("role").default("REPORT").notNull(),
     createdAt: timestamp("created_at", { precision: 6, mode: "string" })
         .notNull()
