8914e7bd740a28c241d08bf5a1d657ffb1a807f2
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2025-10-06 16:57:52 +0900

    Update select salary

--- src/service/salary.service.ts
+++ src/service/salary.service.ts
@@ -1,22 +1,22 @@
 import { and, asc, eq, gte, lte, SQL } from "drizzle-orm";
-import { db, salaryTable, SalaryType, StaffType } from "../db";
-import { LIMIT, STATUS_CODE } from "../constants";
+import { db, salaryTable, SalaryType, staffTable, StaffType } from "../db";
 import { calculateTotalSalary } from "../utils";
+import { LIMIT, STATUS_CODE } from "../constants";
 
-type SalaryWithStaff = SalaryType & { staff: StaffType };
+type SalaryWithStaff = SalaryType & {
+    staffName?: StaffType["name"];
+    salaryType?: StaffType["salaryType"];
+};
 
 const formatResponse = (res: SalaryWithStaff[]) => {
-    const result = res.map(({ staff, ...item }) => ({
+    const result = res.map(item => ({
         ...item,
-        staffName: staff?.name,
-        salaryType: staff?.salaryType,
-        totalSalary: item.salary * item.workingHours + item.target + item.bonus,
-        ...(staff?.salaryType === "MONTHLY" && {
-            workingDays: item.workingDays,
-            actualWorkingDays: item.actualWorkingDays,
-            totalSalary: calculateTotalSalary({
+        total: item.salary * item.workingHours + item.target + item.bonus,
+
+        ...(item.salaryType === "MONTHLY" && {
+            total: calculateTotalSalary({
                 salary: item.salary,
-                paidLeave: item.paidLeave || 0,
+                paidLeave: Number(item.paidLeave) || 0,
                 workingDays: item.workingDays,
                 workingHours: item.workingHours,
                 lunchAllowancePerDay: item.lunchAllowancePerDay,
@@ -27,6 +27,30 @@ const formatResponse = (res: SalaryWithStaff[]) => {
     }));
 
     return result;
+};
+
+const salaryWithStaffSelect = {
+    id: salaryTable.id,
+    staffName: staffTable.name,
+    name: salaryTable.name,
+    salary: salaryTable.salary,
+    salaryType: staffTable.salaryType,
+
+    // HOURLY
+    workingHours: salaryTable.workingHours,
+    target: salaryTable.target,
+    bonus: salaryTable.bonus,
+
+    // MONTHLY
+    workingDays: salaryTable.workingDays,
+    actualWorkingDays: salaryTable.actualWorkingDays,
+    lunchAllowancePerDay: salaryTable.lunchAllowancePerDay,
+    gasolineAllowancePerDay: salaryTable.gasolineAllowancePerDay,
+    paidLeave: salaryTable.paidLeave,
+
+    description: salaryTable.description,
+    startDate: salaryTable.startDate,
+    endDate: salaryTable.endDate,
 };
 
 export const findAllSalary = async ({
@@ -45,26 +69,21 @@ export const findAllSalary = async ({
         whereConditions.push(lte(salaryTable.endDate, endDate));
     }
 
-    return await db.query.salaryTable
-        .findMany({
-            with: {
-                staff: true,
-            },
-            orderBy: asc(salaryTable.name),
-            limit: Number(pageSize),
-            offset: Number(offset),
-            where: and(...whereConditions),
-        })
-        .then((res: SalaryWithStaff[]) => {
+    return await db
+        .select(salaryWithStaffSelect)
+        .from(salaryTable)
+        .leftJoin(staffTable, eq(salaryTable.staffId, staffTable.id))
+        .where(and(...whereConditions))
+        .orderBy(asc(staffTable.name))
+        .limit(Number(pageSize))
+        .offset(Number(offset))
+        .then(res => {
             return {
                 code: STATUS_CODE.SUCCESS,
                 message: "Get Salary successfully!",
-                data: formatResponse(res),
+                data: formatResponse(res as SalaryWithStaff[]),
                 total: res.length,
-                pagination: {
-                    page,
-                    pageSize,
-                },
+                pagination: { page, pageSize },
             };
         });
 };
@@ -79,23 +98,23 @@ export const findSalaryByStaffId = async ({
     const { page = 1, pageSize = LIMIT } = query;
     const offset = (page - 1) * pageSize;
 
-    const salary = await db.query.salaryTable.findMany({
-        with: { staff: true },
-        where: eq(salaryTable.staffId, id),
-        limit: pageSize as never,
-        offset: offset,
-    });
-
-    return {
-        code: STATUS_CODE.SUCCESS,
-        message: "Get Salary by Staff Id successfully!",
-        data: salary ? formatResponse(salary) : [],
-        total: 1,
-        pagination: {
-            page,
-            pageSize,
-        },
-    };
+    return await db
+        .select(salaryWithStaffSelect)
+        .from(salaryTable)
+        .leftJoin(staffTable, eq(salaryTable.staffId, staffTable.id))
+        .where(eq(salaryTable.staffId, id))
+        .orderBy(asc(staffTable.name))
+        .limit(Number(pageSize))
+        .offset(Number(offset))
+        .then(res => {
+            return {
+                code: STATUS_CODE.SUCCESS,
+                message: "Get Salary by Staff Id successfully!",
+                data: formatResponse(res as SalaryWithStaff[]),
+                total: res.length,
+                pagination: { page, pageSize },
+            };
+        });
 };
 
 export const insertSalary = async ({ body }: { body: SalaryType }) => {
